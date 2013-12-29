214e940ba6ee7d0a55302d80a976b259286404aa
Chris Hartjes <chartjes@littlehart.net>
2013-12-29 15:40:33 -0500

    Tweaks and refactors to the Roster model resulting from writing unit tests

--- models/rosters.php
+++ models/rosters.php
@@ -3,59 +3,89 @@ class Roster
 {
     protected $_db;
 
+    /**
+     * Constructor for class
+     *
+     * @param Aura\Sql\Connection\Pgsql $db
+     */
     public function __construct($db)
     {
         $this->_db = $db;
     }
 
+    /**
+     * Get all players on a roster based on the team nicknam
+     *
+     * @param string $nickname
+     * @return array
+     */
     public function getByNickname($nickname)
     {
-    	$sql = "
-    	SELECT *
-    	FROM teams
-    	WHERE ibl_team = '{$nickname}'
-        ORDER BY item_type DESC, tig_name
-    	";
-
-    	$results = $this->_db->query($sql);
+        $select = $this->_db->newSelect();
+        $select->cols('[*]')
+            ->from('teams')
+            ->where('ibl_team = :ibl_team')
+            ->orderBy('item_type DESC, tig_name');
+        $values = ['ibl_team' => $nickname];
+    	$results = $this->_db->fetchAll($select, $values);
 
     	if (!$results) {
-    		return array();
+    		return [];
     	}
 
-    	$roster = array();
+    	$roster = [];
 
-    	while ($results->fetchInto($row, DB_FETCHMODE_ASSOC)) {
+        foreach ($results as $row) {
             $roster[] = $row;
         }
 
         return $roster;
     }
 
+    /**
+     * Update the IBL team a player is on based on the player ID
+     *
+     * @param string $iblTeam
+     * @param integer $playerId
+     * @return boolean
+     */
     public function updatePlayerTeam($iblTeam, $playerId)
     {
-        $sql = "
-        UPDATE teams
-        SET ibl_team = '{$iblTeam}'
-        WHERE id = {$playerId}
-        ";
+        $update = $this->_db->newUpdate();
+        $update->table('teams')
+            ->cols(['ibl_team'])
+            ->where('id = :id');
+        $values = [
+            'ibl_team' => $iblTeam,
+            'id' => $playerId
+        ];
 
-        return $this->_db->query($sql);
+        return $this->_db->query($update, $values);
     }
 
+    /**
+     * Delete a player based on the player ID
+     *
+     * @param integer $player_id
+     * @return boolean
+     */
     public function deletePlayerById($player_id)
     {
-        $bind = ['player_id' => $player_id];
+        $values = ['player_id' => $player_id];
         $delete = $this->_db->newDelete();
         $delete->from('teams')
             ->where('id IN (:player_id)');
 
-        return $this->_db->query($delete, $bind);
+        return $this->_db->query($delete, $values);
     }
 
+    /**
+     * Release players from a team based on an array of ID's
+     *
+     * @param array $release_list
+     */
     public function releasePlayerByList($release_list)
     {
-        $released_player = [];
         $delete = $this->_db->newDelete();
         $delete->from('teams')
             ->where('id = :id');
@@ -68,19 +98,28 @@ class Roster
 
         foreach ($release_list as $release_id) {
             // If a player is uncarded and gets released, we need to delete them
-            $sql = "SELECT status FROM teams WHERE id={$release_id}";
-            $row = $this->_db->fetchOne($sql);
+            $select = $this->_db->newSelect();
+            $select->cols(['status'])
+                ->from('teams')
+                ->where('id = :id');
+            $row = $this->_db->fetchOne($select, ['id' => $release_id]);
             $status = $row["status"];
-            $bind = ['id' => $release_id];
+            $values = ['id' => $release_id];
 
             if ($status == 3) { // player is uncarded, so they get deleted
-                $this->_db->query($delete, $bind);
+                $this->_db->query($delete, $values);
             } else {
-                $this->_db->query($update, $bind);
+                $this->_db->query($update, $values);
             }
         }
     }
 
+    /**
+     * Update an entire roster based on POST data
+     *
+     * @param array $raw_post
+     * @return array
+     */
     public function update($raw_post)
     {
         $activate_list = [];
@@ -155,6 +194,13 @@ class Roster
         ];
     }
 
+    /**
+     * Update an individual player on the roster
+     *
+     * @param array $new_data
+     * @param array $old_data
+     * @return boolean
+     */
     public function updatePlayer($new_data, $old_data)
     {
         $update_list[]="Updating <b>{$new_data['tig_name']}</b><br>";
@@ -173,6 +219,13 @@ class Roster
         return $this->_db->query($update, $bind);
     }
 
+    /**
+     * Add a player not currently on anyone's roster or not a
+     * free agent to someone's roster
+     *
+     * @param array $player_data
+     * @return boolean
+     */
     public function addPlayer($player_data)
     {
         $insert = $this->_db->newInsert();
